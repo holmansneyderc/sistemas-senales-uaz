35e958ed872791852e96a8532add7123b704f194
holman cabezas <holmansneyderc@gmail.com>
2023-10-09 03:04:00 -0600

    vita config

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,26 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-card>
+    <v-layout>
+      <!-- Barra superior con tu nombre, materia y logo de la universidad -->
+      <v-app-bar app color="gray">
+        <!-- Logo de la universidad (ajusta la URL de la imagen según sea necesario) -->
+        <v-img src="@/assets/holman.png" max-height="40" class="mr-4"></v-img>
+
+        <!-- Texto con tu nombre y el nombre de la materia con padding -->
+        <div class="pr-4"> <!-- Agrega un padding a la izquierda (por ejemplo, 4 unidades) -->
+          <div class="caption">Señales y sistemas</div>
+        </div>
+      </v-app-bar>
+
+      <!-- Menú lateral plegable -->
+      <v-navigation-drawer
+        expand-on-hover
+        rail
+      >
+        <v-list>
+          <v-list-item
+            prepend-avatar="@/assets/holman.png"
+            title="Holman Cabezas"
+            subtitle="holmanc@uaz.edu.mx"
+          ></v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-filter-variant" title="Convolución" value="myfiles"></v-list-item>
+          <v-list-item prepend-icon="mdi-chart-line-variant" title="Series de Fourier" value="shared"></v-list-item>
+          <v-list-item prepend-icon="mdi-chart-bell-curve" title="Transformada de Fourier" value="starred"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <!-- Contenido de la página -->
+      <v-main style="height: 250px">
+        <router-view />
+      </v-main>
+    </v-layout>
+  </v-card>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ client/src/components/Menu.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app>
+      <v-list>
+        <v-list-item>
+          <v-list-item-content>
+            <v-menu offset-y>
+              <template v-slot:activator="{ on, attrs }">
+                <v-list-item-title v-bind="attrs" v-on="on">
+                  Opciones
+                </v-list-item-title>
+              </template>
+              <v-list>
+                <v-list-item>
+                  <v-list-item-title>Convolución</v-list-item-title>
+                </v-list-item>
+                <v-list-item>
+                  <v-list-item-title>Discreta</v-list-item-title>
+                </v-list-item>
+                <v-list-item>
+                  <v-list-item-title>Continua</v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Menú</v-toolbar-title>
+    </v-app-bar>
+    <router-view></router-view>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'Menu',
+  data() {
+    return {
+      drawer: false,
+    };
+  },
+}
+</script>
